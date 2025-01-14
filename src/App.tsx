f2630d4b606dd3db4848674ddc6782ad42fd8c41
Nuridin <nurmannuridin28@gmail.com>
2025-01-14 13:16:58 +0500

    adding react-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
-    <div className="text-3xl text-red-800">WILL BE GOOD PROJECT NEXT TIME</div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="text-3xl text-red-800">
+            WILL BE GOOD PROJECT NEXT TIME
+          </div>
+        }
+      />
+      <Route
+        path="/a"
+        element={<div className="text-3xl text-red-800">a</div>}
+      />
+    </Routes>
   );
 }
 
