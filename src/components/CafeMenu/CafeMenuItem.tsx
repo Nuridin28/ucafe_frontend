bba7ad1c73b74cb6d9b9fb163bcfde59b2112db9
Nuridin <nurmannuridin28@gmail.com>
2025-01-31 21:24:39 +0500

    adding redux + basic cart logic on redux

--- src/components/CafeMenu/CafeMenuItem.tsx
+++ src/components/CafeMenu/CafeMenuItem.tsx
@@ -1,5 +1,8 @@
+import { useSelector, useDispatch } from "react-redux";
+
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { cart } from "../../constants/constants";
+import { addToCart } from "../../features/Cart/CartSlice";
 type Props = {
   img: string;
   title: string;
@@ -17,6 +20,10 @@ export default function CafeMenuItem({ img, title, descr, price }: Props) {
     cart.push({ img, title, descr, price });
     console.log(cart);
   };
+
+  const count = useSelector((state: any) => state.cart.count);
+  const dispatch = useDispatch();
+
   return (
     <div className="py-4 sm:py-8 px-4 sm:px-16 border border-gray rounded-2xl mt-4 shadow-lg">
       <div className="flex flex-col sm:flex-row gap-10">
@@ -32,7 +39,14 @@ export default function CafeMenuItem({ img, title, descr, price }: Props) {
           onClick={() => clickOrder(img, title, descr, price)}
           className="cursor-pointer"
         >
-          <AddShoppingCartIcon />
+          {count === 0 ? (
+            <div>
+              <button onClick={() => dispatch(addToCart())}>+</button>
+              <AddShoppingCartIcon />
+            </div>
+          ) : (
+            <button onClick={() => dispatch(addToCart())}>{count}</button>
+          )}
         </div>
       </div>
     </div>
---
+++ src/features/Cart/cartSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state) => {
+      state.count += 1;
+    },
+    removeFromCart: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
