d0d1c62745d3a2751b31187010332b858e0c7bdc
Nuridin <nurmannuridin28@gmail.com>
2025-03-24 18:40:13 +0500

    add dockerfile

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,42 +1,48 @@
-import {postRequest} from "../helpers/api.request.ts";
+import { postRequest } from "../helpers/api.request.ts";
 
 class UserApi {
-    constructor () {
+  constructor() {}
+
+  async register(
+    email: string,
+    password: string,
+    fullName: string,
+    avatarUrl: string
+  ) {
+    try {
+      const data = await postRequest("auth/register", {
+        email,
+        password,
+        fullName,
+        avatarUrl,
+      });
+
+      if (data?.token) {
+        localStorage.setItem("token", data.token);
+        console.log("✅ Токен сохранён!");
+      }
+
+      console.log("Пользователь зарегистрирован:", data);
+      return data;
+    } catch (error) {
+      console.error("Ошибка регистрации:", error);
     }
+  }
 
-    async register(email: string, password: string, fullName: string, avatarUrl: string) {
-        try {
-            const data = await postRequest("auth/register", { email, password, fullName, avatarUrl });
-
-            if (data?.token) {
-                localStorage.setItem("token", data.token);
-                console.log("✅ Токен сохранён!");
-            }
-
-            console.log("Пользователь зарегистрирован:", data);
-            return data;
-        } catch (error) {
-            console.error("Ошибка регистрации:", error);
-        }
+  async login(email: string, password: string) {
+    try {
+      const data = await postRequest("auth/login", { email, password });
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        return data;
+      } else {
+        throw new Error("Некорректный логин или пароль");
+      }
+    } catch (error) {
+      console.error("Ошибка при входе:", error);
+      throw error;
     }
-
-
-    async login(email: string, password: string) {
-        try {
-            const data = await postRequest('auth/login', { email, password });
-            if (data.token) {
-                localStorage.setItem("token", data.token);
-                return data;
-            } else {
-                throw new Error("Некорректный логин или пароль");
-            }
-        } catch (error) {
-            console.error("Ошибка при входе:", error);
-            throw error;
-        }
-    }
-
-
+  }
 }
 
-export const userApi = new UserApi ();
+export const userApi = new UserApi();
